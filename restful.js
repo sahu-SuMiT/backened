4758e1412f8ec9d38b30ef80794ae5e18ce62ee0
SuMiT <145762361+sahu-SuMiT@users.noreply.github.com>
2025-01-26 15:00:15 +0530

    Update restful.js

--- restful.js
+++ restful.js
@@ -1,9 +1,10 @@
 UPDATE:
 GET  /listings/:id/edit -> edit form
-PUT /listings/:id    
+PUT /listings/:id       ->Listing.findByIdAndUpdate(id,object,{validation,new})
 
 POSTING;
 GET /listings/new   -> new form
-POST  /listings
+POST  /listings     ->new Listing(object)  a.save();
 
 DELETE
+DELETE /listings/:id  ->Listing.findByIdAndDelete(id);
